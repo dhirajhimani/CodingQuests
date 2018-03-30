81dca2cb5e094db94295be33d46ee97491274938
dhirajhimani <hdhiraj@thoughtworks.com>
2018-03-30 08:03:56 +0530

    Solved as per hackerrank array display

--- src/main/scala/hackerrank/functionalprogramming/FractalTrees.scala
+++ src/main/scala/hackerrank/functionalprogramming/FractalTrees.scala
@@ -1,5 +1,7 @@
 package hackerrank.functionalprogramming
 
+import java.io.{BufferedWriter, File, FileWriter}
+
 import scala.collection.mutable.ListBuffer
 import hackerrank.functionalprogramming.FunctionalHelpers._
 
@@ -7,7 +9,7 @@ object FractalTrees_1 extends App {
 
   import FractalTrees._
 
-  printFractalTrees(2, 16)
+  printFractalTrees(5, 16)
 }
 
 object FractalTrees {
@@ -26,39 +28,28 @@ object FractalTrees {
       printBoard)()
   }
 
+  private def aBasicPoint: Point = Point(x=63, y=49)
 
-  private def aBasicPoint: Point = {
-    Point(x=63, y=50)
-  }
-
-  private def aBasicBranch: Branch = {
-    Branch(Point(x=63, y=50), Point(x=48, y=50), size = 16)
-  }
-
-  private def newBoard: Board = Array.fill(64)(Array.fill(100)('_'))
+  private def newBoard: Board = Array.fill(63)(Array.fill(100)('_'))
 
   private def splitBranch(N:Int, size:Int): Point => Branches = { startingPoint =>
-    val branchesAndPoints =  splitBranchToThreeSmallerBranches(size, startingPoint)
-    if (N ==0) {
-      branchesAndPoints._1
+    val branches =  splitBranchToThreeSmallerBranches(size)(startingPoint)
+    if (N <= 1) {
+      branches
     } else {
-      branchesAndPoints._1 ++ splitBranch(N-1, size/2)(branchesAndPoints._2(0)) ++ splitBranch(N-1, size/2)(branchesAndPoints._2(1))
+        branches ++ branches.takeRight(2).flatMap({
+          b => splitBranch(N - 1, size / 2)(getEndPointFromBranch(b))
+        })
     }
   }
 
   private def getEndPointFromBranch(branch: Branch): Point = Point(branch.end.x, branch.end.y)
 
-  private def splitBranchToThreeSmallerBranches(size: Int, p: Point): (Branches, Points) = {
-
-    val branches = Seq(
-    Branch(Point(p.x, p.y), Point(p.x - size, p.y), size),
-    Branch(Point(p.x - size, p.y), Point(p.x - 2 * size, p.y + size), size),
-    Branch(Point(p.x - size, p.y), Point(p.x - 2 * size, p.y - size), size))
-    val points = Seq(
-      getEndPointFromBranch(branches(1)),
-      getEndPointFromBranch(branches(2))
-    )
-    (branches,points)
+  private def splitBranchToThreeSmallerBranches(size: Int): Point => Branches = { p: Point =>
+    Seq(
+      Branch(Point(p.x, p.y), Point(p.x - size, p.y), size),
+      Branch(Point(p.x - size, p.y), Point(p.x - 2 * size, p.y + size), size),
+      Branch(Point(p.x - size, p.y), Point(p.x - 2 * size, p.y - size), size))
   }
 
   private def extractPointsFromBranches: Branches => Points =  _ flatMap getColoredPointsFromABranch
@@ -67,7 +58,7 @@ object FractalTrees {
     var counter = if (branch.begin.y > branch.end.y) -1 else 1
     if (branch.begin.y == branch.end.y) {
       for {
-        x <- (branch.end.x) to (branch.begin.x)
+        x <- (branch.end.x) until (branch.begin.x)
       } yield Point(x, branch.begin.y)
     } else {
       val points = new ListBuffer[Point]()
@@ -85,7 +76,17 @@ object FractalTrees {
     }
   }
 
-  private def printBoard: Board => Unit = { board => for(row <- board) println(row.mkString) }
+  private def printBoard: Board => Unit = {
+    board =>
+      val file = new File("TempFileToMatchChallengeArray.txt")
+      val bw = new BufferedWriter(new FileWriter(file))
+      for(row <- board) {
+        val line = row.mkString
+        println(line)
+        bw.write(line + "\n")
+      }
+      bw.close()
+  }
 
 }
 
