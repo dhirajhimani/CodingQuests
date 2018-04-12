f7ae798d5cfd17bd2143e9cd8c7647a2e5c4a333
dhirajhimani <hdhiraj@thoughtworks.com>
2018-04-12 19:30:03 +0530

    Solved Hackerland Radio Transmitters using Greedy Algo

---
+++ src/main/scala/hackerrank/algorithims/search/HackerlandRadioTransmitters_UsingGreedy.scala
@@ -0,0 +1,30 @@
+package hackerrank.algorithims.search
+
+import scala.collection.mutable
+
+//https://www.hackerrank.com/challenges/hackerland-radio-transmitters/problem
+// Uses lots of memory, timeout Exception
+object HackerlandRadioTransmitters_UsingGreedy extends App {
+
+    println(hackerlandRadioTransmitters(Array(1,2,3,4,5), 1))
+  println(hackerlandRadioTransmitters(Array(7, 2, 4, 6, 5, 9, 12, 11), 2))
+  println(hackerlandRadioTransmitters(Array(1, 4, 5, 7), 2))
+  println(hackerlandRadioTransmitters(Array(1, 8), 1))
+
+  def hackerlandRadioTransmitters(x: Array[Int], k: Int): Int = {
+    scala.util.Sorting.quickSort(x)
+    var numOfTransmitters = 0;
+    var i = 0;
+    val n = x.size
+    while (i < n) {
+      numOfTransmitters += 1;
+      var loc = x(i) + k;
+      while (i < n && x(i) <= loc) i += 1;
+      i -= 1
+      loc = x(i) + k;
+      while (i < n && x(i) <= loc) i += 1;
+    }
+    numOfTransmitters;
+  }
+
+}
